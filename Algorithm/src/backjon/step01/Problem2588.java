a0d94829475df796654a83770cdc241eebd8afe2
GoRyeoJin <sooon9254@naver.com>
2022-04-26 12:19:32 +0900

    2588 풀이 2가지 추가, 1330 문제 풀이

--- Algorithm/src/backjon/step01/Problem2588.java
+++ Algorithm/src/backjon/step01/Problem2588.java
@@ -1,26 +1,73 @@
 package backjon.step01;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Problem2588 {
-
-	public static void main(String[] args) {
-		
+	
+	public static void main(String[] args) throws IOException {
+		/* 내가 처음 푼 방식
 		Scanner sc = new Scanner(System.in);
 		
-		String num1 = sc.nextLine();
-		String num2 = sc.nextLine();
+		int A = Integer.parseInt(sc.nextLine());
+		String B = sc.nextLine();
 		
 		sc.close();
 		
-		int var4 = Integer.parseInt(String.valueOf(num2.charAt(0)));
-		int var5 = Integer.parseInt(String.valueOf(num2.charAt(1)));
-		int var6 = Integer.parseInt(String.valueOf(num2.charAt(2)));
-		System.out.println(472%10);
-		System.out.println(Integer.parseInt(num1) * var6);
-		System.out.println(Integer.parseInt(num1) * var5);
-		System.out.println(Integer.parseInt(num1) * var4);
-		System.out.println(Integer.parseInt(num1) * Integer.parseInt(num2));
+		int B1 = Integer.parseInt(String.valueOf(B.charAt(0)));
+		int B2 = Integer.parseInt(String.valueOf(B.charAt(1)));
+		int B3 = Integer.parseInt(String.valueOf(B.charAt(2)));
+		
+		System.out.println(A * B3);
+		System.out.println(A * B2);
+		System.out.println(A * B1);
+		System.out.println(A * Integer.parseInt(B));
+		
+		*/
+		
+		// 1. charAt() 사용
+		/*
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		String B = sc.next();
+		
+		sc.close();
+		
+		System.out.println(A * (B.charAt(2) - '0'));
+		System.out.println(A * (B.charAt(1) - '0'));
+		System.out.println(A * (B.charAt(0) - '0'));
+		System.out.println(A * Integer.parseInt(B));
+		*/
+		
+		// 2. 숫자를 수학적으로 접근
+		/*
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.println(A*(B%10));
+		System.out.println(A*(B%100/10));
+		System.out.println(A*(B/100));
+		System.out.println(A*B);
+		*/
+		
+		// 3. toCharArray() 사용
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int A = Integer.parseInt(br.readLine());
+		String B = br.readLine();
+		
+		char[] b = B.toCharArray();
+		
+		System.out.println(A * (b[2]-'0'));
+		System.out.println(A * (b[1]-'0'));
+		System.out.println(A * (b[0]-'0'));
+		System.out.println(A * Integer.parseInt(B));
 	}
 
 }
